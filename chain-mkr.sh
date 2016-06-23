352f3eb27bb3b9814c1c37edba899cdbc7a28e76
Daniel Lowrie <dlowrie290@yahoo.com>
2016-06-22 22:05:37 -0400

    Update chain-mkr.sh

--- chain-mkr.sh
+++ chain-mkr.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
+Main() {
+echo
+echo
+echo "Create new Rule (C), Delete Rule (D), Exit (X)"
+echo
+read CHOICE
+echo
+if [[ $CHOICE == [C,c] ]] ; then
+  while [ $VAR1 -eq 1 ]
+    do
+      Create_Chain
+    done
+elif [[ $CHOICE == [D,d] ]] ; then
+  while [ $VAR2 -eq 1 ]
+    do
+      Del_Chain
+    done
+elif [[ $CHOICE == [X,x] ]] ; then
+  echo "Goodbye!"
+  exit
+else
+  echo "Invalid Option! Try again."
+}
+
+
 Create_Chain() {
   echo "Which Chain? INPUT (I), FORWARD (F), OUTPUT (O), Exit (X)"
   echo
@@ -9,77 +34,77 @@ Create_Chain() {
     echo "Creating rule for INPUT Chain"
     CHAIN_1="iptables -I INPUT 1 "
     ((VAR1++))
+    Main
   elif [[ $CHAIN == [F,f] ]] ; then
     echo "Creating rule for FORWARD Chain"
     CHAIN_1="iptables -I FORWARD 1 "
     ((VAR1++))
+    Main
   elif [[ $CHAIN == [O,o] ]] ; then
     echo "Creating rule for OUTPUT Chain"
     CHAIN_1="iptables -I OUTPUT 1 "
     ((VAR1++))
+    Main
   elif [[ $CHAIN == [X,x] ]] ; then
     ((VAR1++))
+    echo "Exiting Chain Creation, back to Main Menu..."
+    Main
   else
     echo "Invalid Selction! Please Try again"
     Create_Chain
   fi
 }
 
-Edit_Chain() {
+Del_Rule() {
   echo "Which Chain? INPUT (I), FORWARD (F), OUTPUT (O), Exit (X)"
   echo
   read SELECT
   echo
     if [[ $SELECT == [I,i] ]] ; then
-    EDIT_CHAIN="INPUT"
+    DEL_CHAIN="INPUT"
     elif [[ $SELECT == [F,f] ]] ; then
-    EDIT_CHAIN="FORWARD"
+    DEL_CHAIN="FORWARD"
     elif [[ $SELECT == [O,o] ]] ; then
-    EDIT_CHAIN="OUTPUT"
+    DEL_CHAIN="OUTPUT"
     elif [[ $SELECT == [X,x] ]] ; then
       ((VAR2++))
+      echo "Exiting Rule Deletion, back to Main Menu..."
+      Main
     else
       echo "Invalid option. Try again"
-      Edit_Chain
+      Del_Chain
     fi
-  iptables -L $EDIT_CHAIN --line-numbers
+  iptables -L $DEL_CHAIN --line-numbers
   echo
-  echo "Which rule would you like to edit?"
+  echo "Which rule would you like to delete?"
   echo 
   read RULE_NUM
   echo
-  iptables -L $EDIT_CHAIN --line-numbers | grep ^$RULE_NUM > /tmp/iptables_rule_copy.txt 
+  iptables -L $DEL_CHAIN --line-numbers | grep ^$RULE_NUM >> /tmp/iptables_edit_rule.log
+  iptables -D $DEL_CHAIN $RULE_NUM
+  iptables -L $DEL_CHAIN
+  echo "***Rule successfully Deleted***"
+}
+
+Protocol() {
+  echo
+  echo "Which protocol? TCP (T), UDP (U), ICMP (I), or Exit (X)"
+  echo
+  read INPUT
+  if [[ $INPUT == [T,t] ]] ; then
+    PROT1="-p tcp"
+    ()
 }
 
 VAR1=1
 VAR2=1
-VAR3=1
-PROT1=1
-STATE1=1
 
-echo
-echo "Create new Rule (C), Edit Existing Rule (E), Delete Rule (D), Exit (X)"
-echo
-read CHOICE
-echo
-if [[ $CHOICE == [C,c] ]] ; then
-  while [ $VAR1 -eq 1 ]
-    do
-      Create_Chain
-    done
-elif [[ $CHOICE == [E,e] ]] ; then
-  while [ $VAR2 -eq 1 ]
-    do
-      Edit_Chain
-    done
-elif [[ $CHOICE == [D,d] ]] ; then
-  while [ $VAR3 -eq 1 ]
-    do
-      Delete_Chain
-    done
-elif [[ $CHOICE == [X,x] ]] ; then
-  exit
-else
+PROT1="protocol"
+STATE1="state"
+CHAIN_1="chain"
+DEL_CHAIN="del_chain"
+
+Main
   
 #    echo
 #    echo "Which protocol? TCP (T), UDP (U), or ICMP (I)
